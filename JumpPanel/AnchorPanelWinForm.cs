0959b0a4834b3ab8736e1216f3f6400827af042f
garso <garso@DESKTOP-1VV6Q6R>
2017-07-07 09:12:30 +0800

    添加项目文件。

---
+++ JumpPanel/AnchorPanelWinForm.cs
@@ -0,0 +1,242 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JumpPanel
+{
+
+    public class AnchorPanel
+    {
+        List<PanelMenu> lst = new List<PanelMenu>();
+
+        Control MenuPan { get; set; }
+
+        ScrollableControl XSControl;
+
+        public AnchorPanel(Panel PanMenu, ScrollableControl xtraScrollableControl)
+        {
+            
+            MenuPan = PanMenu;
+            XSControl = xtraScrollableControl;
+            XSControl.Scroll += XSControl_Scroll;
+            XSControl.MouseWheel += XSControl_MouseWheel;
+            XSControl.SizeChanged += XSControl_SizeChanged;
+            XSControl.VerticalScroll.LargeChange = 20;
+        }
+
+
+        void XSControl_SizeChanged(object sender, EventArgs e)
+        {
+            if (LastAnchor != null && LastAnchorIniHeight < (sender as Control).Height)
+            {
+                LastAnchor.AnchorContainer.Height = (sender as Control).Height;
+            }
+        }
+
+
+        #region 容器滚动条移动事件
+        void XSControl_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+            XSControl_Scroll(sender, null);
+        }
+
+        void XSControl_Scroll(object sender, ScrollEventArgs e)
+        {
+            CurrentLable = GetMenu((sender as ScrollableControl).VerticalScroll.Value);
+        }
+        #endregion
+
+        #region 添加锚点
+
+        PanelMenu LastAnchor;
+        int LastAnchorIniHeight;
+
+        /// <summary>
+        /// 添加锚点
+        /// </summary>
+        /// <param name="col">默认为控件的Top，Height,Text属性</param>
+        /// <param name="LastControl">是否是最后一一个锚点，为了保证最后一个锚点定位在顶部，需要动态设置最后一个锚点的高度，如果最后一个锚点区域高度小于容器高度，则设置其高度为容器高度</param>
+        public void AddAnchor(Control col, bool LastControl)
+        {
+            AddAnchor(col, col.Text, LastControl);
+        }
+        /// <summary>
+        /// 添加锚点
+        /// </summary>
+        /// <param name="col">默认为控件的Top，Height属性</param>
+        /// <param name="Caption">如果Caption为空则取Col的Text属性</param>
+        /// <param name="LastControl">是否是最后一一个锚点，为了保证最后一个锚点定位在顶部，需要动态设置最后一个锚点的高度，如果最后一个锚点区域高度小于容器高度，则设置其高度为容器高度</param>
+        public void AddAnchor(Control col, string Caption, bool LastControl)
+        {
+
+            Label lbl = new Label()
+            {
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Location = new System.Drawing.Point(0, 0),
+                /*lbl.Size = new System.Drawing.Size(219, 37);*/
+                Height = 37,
+                TabIndex = 0,
+                Text = Caption,
+                TextAlign = System.Drawing.ContentAlignment.MiddleRight,
+                Tag = col.Top.ToString()
+            };
+
+            IniEventLable(lbl);
+            if (LastControl)
+            {
+                LastAnchor = new PanelMenu(lbl, col);
+                LastAnchorIniHeight = col.Height;
+                lst.Add(LastAnchor);
+            }
+            else
+                lst.Add(new PanelMenu(lbl, col));
+
+            MenuPan.Controls.Add(lbl);
+            MenuPan.Controls.SetChildIndex(lbl, 0);
+
+        }
+
+
+        #endregion
+
+        /// <summary>
+        /// 根据滚动条位置获得对应的锚点空间
+        /// </summary>
+        /// <param name="ScrollValue">滚动条的值</param>
+        /// <returns></returns>
+        public Label GetMenu(int ScrollValue)
+        {
+            Label lbl = null;
+            foreach (PanelMenu menu in lst)
+            {
+                if (menu.Top <= ScrollValue && menu.Buttom > ScrollValue)
+                    lbl = menu.Label;
+            }
+            if (lbl == null)
+            {
+                return null;
+            }
+            return lbl;
+        }
+
+        /// <summary>
+        /// 初始化锚点的事件
+        /// </summary>
+        /// <param name="lbl"></param>
+        void IniEventLable(Label lbl)
+        {
+            lbl.MouseEnter += lbl_MouseEnter;
+            lbl.MouseLeave += lbl_MouseLeave;
+
+            lbl.MouseClick += lbl_MouseClick;
+        }
+
+        #region 锚点单击
+        Label _CurrentLable;
+        public Label CurrentLable
+        {
+            set
+            {
+                if (value == null) return;
+                if (_CurrentLable == value) return;
+                value.BackColor = Color.LightPink;
+                if (_CurrentLable != null)
+                    _CurrentLable.BackColor = Color.Transparent;
+                _CurrentLable = value;
+            }
+            get { return _CurrentLable; } //{ return CurrentLable; }
+        }
+
+        /// <summary>
+        /// 鼠标点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void lbl_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                CurrentLable = sender as Label;
+
+                XSControl.VerticalScroll.Value = int.Parse((sender as Label).Tag.ToString()) - CurrentLable.Top;
+            }
+        }
+
+        /// <summary>
+        /// 设置鼠标进入时背景色
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            if ((sender as Label) != CurrentLable)
+                (sender as Label).BackColor = Color.FromArgb(0xFF, 0xFF, 0x99);
+        }
+
+        /// <summary>
+        /// 鼠标移出，还原背景色
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            if ((sender as Label) != CurrentLable)
+                (sender as Label).BackColor = Color.Transparent;
+        }
+        #endregion
+    }
+
+    public class PanelMenu
+    {
+        public PanelMenu(Label label, Control anchorContainer)
+        {
+            Label = label;
+            AnchorContainer = anchorContainer;
+            Top = anchorContainer.Top;
+        }
+
+        public PanelMenu(Label label, int top, int height)
+        {
+            Label = label;
+            Top = top;
+            Height = height;
+        }
+
+        /// <summary>
+        /// 锚点定位的容器对象，通常是Panel
+        /// </summary>
+        public Control AnchorContainer { get; set; }
+        /// <summary>
+        /// 锚点，Lable
+        /// </summary>
+        public Label Label { get; set; }
+
+
+        public int Top
+        {
+            get;
+            set;
+        }
+        private int _height;
+        public int Height
+        {
+            get
+            {
+                if (AnchorContainer != null)
+                    return AnchorContainer.Height;
+                else
+                    return _height;
+            }
+            set { _height = value; }
+        }
+
+        public int Buttom { get { return Top + Height; } }
+    }
+}
---
+++ JumpPanel/Form3.designer.cs
@@ -0,0 +1,272 @@
+﻿namespace JumpPanel
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(739, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.panel9);
+            this.panel2.Controls.Add(this.panel8);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(138, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(285, 529);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel9.Controls.Add(this.label8);
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(0, 1197);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(268, 100);
+            this.panel9.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(59, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "label8";
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label7);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(0, 1097);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(268, 100);
+            this.panel8.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "label7";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel7.Controls.Add(this.label6);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 856);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(268, 241);
+            this.panel7.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label6";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 580);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(268, 276);
+            this.panel6.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label5";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 439);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(268, 141);
+            this.panel5.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label4";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 100);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(268, 339);
+            this.panel4.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(268, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // panel10
+            // 
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel10.Location = new System.Drawing.Point(0, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(138, 529);
+            this.panel10.TabIndex = 9;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 529);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel10;
+    }
+}
